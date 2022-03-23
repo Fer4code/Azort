5df2d61edcd4469749e16f7895366435308107d3
Fernando <54428180+F3RN4ND02@users.noreply.github.com>
2022-03-23 15:51:56 -0400

    About Us improvement

--- src/components/parts/AboutUs.js
+++ src/components/parts/AboutUs.js
@@ -20,31 +20,31 @@ function AboutUs() {
 						<Grid item xs={12} md={12} align='center' className='item'>
 							<Typography variant="h3" color="initial">Misión</Typography>
 							<Typography variant="h5" color="initial" align='justify'> 
-							Compartir el asesoramiento global y materias primas con el mercado industrial nacional,
-        					que contribuya al mejoramiento de la calidad, reduccion de costos, proteccion del talento
-        					humano, disminucion del impacto en el medio ambiente y en alianza, propiciar el desarrollo
-        					industrial del pais.
+							Compartir el asesoramiento global y materias primas con el mercado industrial nacional, que les
+							apoye en el mejoramiento de la calidad, reducción de costos, protección del talento humano y
+							disminución del impacto en el medio ambiente y en alianza, propiciar el desarrollo industrial del país.
 						</Typography>
                        </Grid>
 						<Grid item xs={12} md={12} align='center' className='item'>
                         <Typography variant="h3" color="initial">Visión</Typography>
 						<Typography variant="h5" color="initial" align='justify'>
-							Ser los aliados comerciales industriales mas confiables, dentro y fuera del mercado venezolano,
-        					comprometidos con nuestros valores y el desarrollo del pais.</Typography>
+						Ser los aliados comerciales industriales más confiables en el mercado venezolano,
+						comprometidos con nuestros valores y el desarrollo del país.
+						</Typography>
                             </Grid>
 							
 						<Grid item xs={12} md={12} align='center' className='item'>
 							<Typography variant="h3" color="initial">Nuestros Valores</Typography>
 							<Typography variant="h5" color="initial">
-		Responsabilidad; cumplir con lo acordado,
-        Compromiso; sentimos pasion por lo que hacemos, brindar mas,
-        Honestidad; transparencia en el desempeño, decir las cosas como son,
-        Respeto; todos somos valiosos, todos tenemos talent y eso se respeta, trato cordial,
-        Innovacion; siempre hay una mejor forma de hacerlo.. reinventar es indispensable,
-        Conexion: sensibilidad con la realidad del pais y del mercado, los pies en la tierra,
-        Talento; generar alianzas positivas, en conjunto tenemos mas oportunidades de exito,
-        Impulso; vamos por mas.</Typography>
-                         
+							Responsabilidad; cumplir con lo acordado,
+							Compromiso; sentimos pasión por lo que hacemos, brindar más,
+							Honestidad; transparencia en el desempeño, decir las cosas como son,
+							Respeto; todos somos valiosos, todos tenemos talento y eso se respeta, trato cordial,
+							Innovación; siempre hay una mejor forma de hacerlo…re inventar es indispensable,
+							Conexión; sensibilidad con la realidad del país y del mercado, los pies en la tierra,
+							Talento; generar alianzas positivas, en conjunto tenemos más oportunidades de éxito,
+							Impulso; vamos por más.
+							</Typography>
 						</Grid>
 						
 					</Grid>
